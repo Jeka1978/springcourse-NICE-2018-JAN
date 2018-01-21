a1ac20c5f8bd8e2925371edc815ff8873451b206
bsevgeny@gmail.com <papakita2009>
2018-01-21 16:09:55 +0200

    spring second lab solution

--- src/main/java/quoters/TalkingRobotImpl.java
+++ src/main/java/quoters/TalkingRobotImpl.java
@@ -1,12 +1,20 @@
 package quoters;
 
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 /**
  * @author Evgeny Borisov
  */
+@Setter
+@RequiredArgsConstructor
 public class TalkingRobotImpl implements TalkingRobot {
-    private List<Quoter> quoters;
+    private final List<Quoter> quoters;
+
+
     @Override
     public void talk() {
        quoters.forEach(Quoter::sayQuote);
